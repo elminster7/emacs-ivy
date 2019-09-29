5e728dcbaea3dd5e1d2731782c81c9ff12846740
babel <babel>
2019-09-30 02:46:51 +0900

    ecb version default save

--- init.el
+++ init.el
@@ -29,6 +29,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ecb-options-version "2.50")
  '(package-selected-packages
    (quote
     (ecb minibuf-isearch neotree ripgrep elgrep popup-imenu e2wm multiple-cursors xcscope counsel-gtags ivy use-package))))
