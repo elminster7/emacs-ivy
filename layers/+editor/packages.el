3fa966cb332279393e42a2db48d0138493aa4950
elminster <kamase79@daum.net>
2020-03-27 00:26:41 +0900

    dummy lsp mode

--- layers/+editor/packages.el
+++ layers/+editor/packages.el
@@ -41,27 +41,58 @@
     ))
 
 ;; lsp mode
-(defun editor/lsp-package ()
+(defun editor/lsp-mode ()
   "lsp packages"
-  (use-package lsp-ui
-    :ensure t
-    :requires use-package-hydra
-    :commands lsp-ui-mode
+  (use-package lsp-mode
+    :defer t
+    :commands lsp
+    :custom
+    (lsp-auto-guess-root t)
+    (lsp-enable-snippet nil)
+    (lsp-prefer-flymake nil) ; Use flycheck instead of flymake
+    :bind (:map lsp-mode-map ("C-c C-f" . lsp-format-buffer))
+    :hook ((python-mode c-mode c++-mode) . lsp)
     :config
-    (setq lsp-ui-sideline-enable t)
-    (setq lsp-print-io nil)
-    (setq lsp-prefer-flymake :none)
-    (setq flycheck-checker-error-threshold 10000)
-    (setq lsp-ui-flycheck-enable t)
-    (setq-local flycheck-checker 'python-flake8)
-    (setq lsp-ui-flycheck-list-position 'right)
-    (setq lsp-ui-flycheck-live-reporting t)
-    (setq lsp-ui-peek-enable t)
-    (setq lsp-ui-peek-list-width 60)
-    (setq lsp-ui-peek-peek-height 25)
-    (setq lsp-ui-imenu-enable t)
-    (setq lsp-ui-doc-enable t)
-    (setq lsp-ui-sideline-ignore-duplicate t))
+    (setq lsp-prefer-flymake nil) ;; Prefer using lsp-ui (flycheck) over flymake.
+    ))
+  ;; :config (setq lsp-clients-clangd-args '("-j=4" "-background-index" "-log=error"))
+
+(defun editor/lsp-ui ()
+  "lsp mode"
+  (use-package lsp-ui
+    :requires lsp-mode flycheck
+    :commands lsp-ui-mode
+    :custom-face
+    (lsp-ui-doc-background ((t (:background nil))))
+    (lsp-ui-doc-header ((t (:inherit (font-lock-string-face italic)))))
+    :bind (:map lsp-ui-mode-map
+		([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
+		([remap xref-find-references] . lsp-ui-peek-find-references)
+		("C-c u" . lsp-ui-imenu))
+    :hook (lsp-mode-hook . lsp-ui-mode)
+    :custom
+    (lsp-ui-doc-enable t)
+    (lsp-ui-doc-use-childframe t)
+    (lsp-ui-flycheck-enable t)
+    (lsp-ui-flycheck-list-position 'right)
+    (lsp-ui-flycheck-live-reporting t)
+    (lsp-ui-peek-enable t)
+    (lsp-ui-peek-list-width 60)
+    (lsp-ui-peek-peek-height 25)
+    (lsp-ui-doc-header t)
+    (lsp-ui-doc-include-signature t)
+    (lsp-ui-doc-position 'top)
+    (lsp-ui-doc-border (face-foreground 'default))
+    (lsp-ui-sideline-enable t)
+    (lsp-ui-sideline-ignore-duplicate t)
+    (lsp-ui-sideline-show-code-actions t)
+    :config
+    ;; Use lsp-ui-doc-webkit only in GUI
+    (setq lsp-ui-doc-use-webkit nil)
+    ;; WORKAROUND Hide mode-line of the lsp-ui-imenu buffer
+    ;; emacs-lsp/lsp-ui#243
+    (defadvice lsp-ui-imenu (after hide-lsp-ui-imenu-mode-line activate)
+      (setq mode-line-format nil)))
   )
 
 ;; company rtags
@@ -406,7 +437,8 @@
   (editor/default-env)
   (editor/hs-minor-mode)
   (editor/white-space)
-  (editor/lsp-package)
+  (editor/lsp-mode)
+  (editor/lsp-ui)
   )
 
 (defun editor/dired-settings ()
